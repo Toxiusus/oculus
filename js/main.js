ae7eeb047ec1f2e4472e189f0ed8665f55d80c24
Toxiusus <icek1lla2323@gmail.com>
2023-12-05 20:27:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const tabItem = document .querySelectorAll('.tabs__btn-item')
+const tabContent = document .querySelectorAll('.tabs__content-iten')
+
+tabItem.forEach(function(element){
+    element.addEventListener('click', open);
+});
+
+function open(evt){
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function(item){
+        item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-iten--active');    
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+    document.querySelector(`#${button}`).classList.add('tabs__content-iten--active');
+}
+
